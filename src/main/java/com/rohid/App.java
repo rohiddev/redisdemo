019be65a23458f48d743f0a4241690d13e35fc35
rohid dev <rohiddev@rohids-MacBook-Air.local>
2020-01-15 10:16:06 -0700

    update

---
+++ src/main/java/com/rohid/App.java
@@ -0,0 +1,34 @@
+package com.rohid;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.rohid.model.Item;
+
+@SpringBootApplication
+public class App {
+
+	@Bean
+	public JedisConnectionFactory connectionFactory() {
+	  JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+	  connectionFactory.setHostName("localhost");
+	  connectionFactory.setPort(6379);
+	 
+	  return connectionFactory;
+	}
+
+	@Bean
+	RedisTemplate<String, Item> redisTemplate(){
+		RedisTemplate<String,Item> redisTemplate = new RedisTemplate<String, Item>();
+		redisTemplate.setConnectionFactory(connectionFactory());
+		return redisTemplate;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
